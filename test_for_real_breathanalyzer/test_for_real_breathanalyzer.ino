c53f10ffa19c8a387be39a4563bc372a050971ad
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2023-08-05 00:57:01 +0300

    Update test_for_real_breathanalyzer.ino

--- test_for_real_breathanalyzer/test_for_real_breathanalyzer.ino
+++ test_for_real_breathanalyzer/test_for_real_breathanalyzer.ino
@@ -1,7 +1,7 @@
 // float sensor_volt;
 // float RS_gas;
 // float R0;
-// int R2 = 1.02;
+// int R2 = 1000;
 
 // void setup() {
 //  Serial.begin(9600);
@@ -23,6 +23,7 @@
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64
 #define OLED_RESET 4
+#define BUTTON_PIN 4
 Adafruit_SH1106 display(OLED_RESET);
 
 
@@ -45,49 +46,75 @@ void setup() {
   Serial.println("MQ3 warming up!");
   display.println("MQ3 warming up!");
   display.display();
-  delay(20000); // allow the MQ3 to warm up
+  delay(2000);  // allow the MQ3 to warm up
   display.clearDisplay();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
-//    byte buttonState = digitalRead(BUTTON_PIN);
-  
-//   if (buttonState == LOW) {
-//       Serial.println("Button is pressed");
-//   }
-//   else {
-//       Serial.println("Button is not pressed");
-//   }
-//   delay(100);
-// void realTesting(){
-
-// }
+byte buttonState;
+bool realTesting() {
+buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == LOW) {
+    Serial.println("Button is pressed");
+    return true;
+  } else {
+    Serial.println("Button is not pressed");
+    return false;
+  }
+  delay(100);
+}
 
 void loop() {
+   display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 32);
+    // display.print("PPM ");
+    display.print("Press the button to start measuring! ");
+    display.display();
+  if (realTesting()) {
+      display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 32);
+    // display.print("PPM ");
+    display.print("Measuring...");
+    display.display();
+    delay(3000);
+    while(true){
+      //
+      //
+      display.clearDisplay();
+
+
+    int sensorValue = analogRead(A0);
+    Serial.println(sensorValue);
+    sensor_volt = (float)sensorValue / 1024 * 5.0;
+    RS_gas = ((5.0 * R2) / sensor_volt) - R2;
+    /*-Replace the value of R0 with the value of R0 in your test -*/
+    // R0 = 72.69;
+    R0 = 36.23;
+    ratio = RS_gas / R0;  // ratio = RS/R0
+    double x = 0.4 * ratio;
+    BAC = pow(x, -1.431);  //BAC in mg/L
+    Serial.print("BAC = ");
+    Serial.print(BAC * 0.0001);  //convert to g/dL
+    Serial.print(" g/DL\n\n");
+    display.clearDisplay();
+    display.setTextSize(1.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 32);
+    // display.print("PPM ");
+    display.print("BAC = ");
+    display.print(BAC * 0.0001);
+    display.print(" g/DL\n\n");
+    display.display();
+    delay(1000);
+    }
+    
+
+  } else {
+    Serial.print("Nothing");
+  }
   // realTesting();
-  display.clearDisplay();
-  
-
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  sensor_volt = (float)sensorValue / 1024 * 5.0;
-  RS_gas = ((5.0 * R2) / sensor_volt) - R2;
-  /*-Replace the value of R0 with the value of R0 in your test -*/
-  R0 = 0.09;
-  ratio = RS_gas / R0;  // ratio = RS/R0
-  double x = 0.4 * ratio;
-  BAC = pow(x, -1.431);  //BAC in mg/L
-  Serial.print("BAC = ");
-  Serial.print(BAC * 0.0001);  //convert to g/dL
-  Serial.print(" g/DL\n\n");
-  display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0,32);
-  // display.print("PPM ");
-  display.print("BAC = ");
-  display.print(BAC * 0.0001);
-  display.print(" g/DL\n\n");
-  display.display();
-  delay(1000);
 }
-
