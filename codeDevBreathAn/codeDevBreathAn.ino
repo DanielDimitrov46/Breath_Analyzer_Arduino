f6281181003da4568daa127cfc7261c059960907
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2023-09-02 01:04:14 +0300

    Before final editing

--- codeDevBreathAn/codeDevBreathAn.ino
+++ codeDevBreathAn/codeDevBreathAn.ino
@@ -5,15 +5,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
 
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
+//define ports and display for further use
+#define SCREEN_WIDTH 128  
 #define SCREEN_HEIGHT 64
 #define OLED_RESET 4
 #define BUTTON_PIN 4
-#define BUZZER_PIN 8  // Change this to the actual pin connected to the passive buzzer
-
+#define BUTTON_PIN2 2
+#define BUZZER_PIN 8  
 
 Adafruit_SH1106 display(OLED_RESET);
-
+// SoftwareSerial arduinoSerial(3, 2);
 float sensor_volt;
 float RS_gas;
 float R0;
@@ -22,10 +23,18 @@ float BAC;
 int R2 = 1.02;
 
 const unsigned long MEASURING_DURATION = 5000;  // Measuring duration in milliseconds (5 seconds)
-const unsigned long ONE_MINUTE = 60000;         // One minute in milliseconds
+// const unsigned long ONE_MINUTE = 60000;         // One minute in milliseconds
 bool measuringInProgress = false;               // Flag to track if measuring is in progress
 bool measurementCompleted = false;              // Flag to track if measurement is completed
 unsigned long measuringStartTime;
+
+//kod bateriq
+const int batteryPin = A0;  // Аналогов пин за измерване на напрежението на батерията
+
+// Спецификации на батерията
+const float chargingLimitVoltage = 4.2;  // Максимално напрежение при зареждане
+const float ratedVoltage = 3.7;          // Номинално напрежение
+
 
 // Function to initialize the OLED display
 void setupDisplay() {
@@ -36,13 +45,39 @@ void setupDisplay() {
   display.setCursor(0, 32);
   display.println("MQ3 warming up!");
   display.display();
-  delay(2000);
+  delay(2000); // da se opravi za realnata rabota
   display.clearDisplay();
 }
 
+void batteryLevel() {
+  int rawValue = analogRead(batteryPin);      // Четене на стойността от аналоговия пин
+  float voltage = (rawValue / 1023.0) * 5.0;  // Преобразуване на стойността в напрежение (при 5V референтно напрежение)
+
+  // Пресмятане на процентите на батерията
+  float batteryPercentage = ((voltage - ratedVoltage) / (chargingLimitVoltage - ratedVoltage)) * 100.0;
+
+  if (batteryPercentage < 0.00) {
+    batteryPercentage = 0.00;
+  }
+  display.clearDisplay();
+  display.setTextSize(1.5);
+  display.setCursor(5, 32);
+  display.println("Battery:");
+  display.print(batteryPercentage);
+  display.println("%");
+  display.println(voltage);
+  display.display();
+  // delay(2000);
+  display.clearDisplay();
+  delay(5000);
+}
+
 // Function to check if the button is pressed to start measuring
-bool isButtonPressed() {
+bool isButtonPressed1() {
   return (digitalRead(BUTTON_PIN) == LOW);
+}
+bool isButtonPressed2() {
+  return (digitalRead(BUTTON_PIN2) == LOW);
 }
 
 // Function to perform the actual measurement and calculate BAC
@@ -53,7 +88,7 @@ float measureBAC() {
   while ((elapsedTime = millis() - measuringStartTime) < MEASURING_DURATION) {
     tone(BUZZER_PIN, 3000);
     // delay(duration);
-
+    
     display.clearDisplay();
     display.setTextSize(1.5);
     display.setTextColor(WHITE);
@@ -65,14 +100,14 @@ float measureBAC() {
   noTone(BUZZER_PIN);
 
   // Calculate the BAC after 5 seconds of measuring
-  sensorValue = analogRead(A0);
+  sensorValue = analogRead(A1);
   Serial.println(sensorValue);
   sensor_volt = (float)sensorValue / 1024 * 5.0;
   RS_gas = ((5.0 * R2) / sensor_volt) - R2;
   /*-Replace the value of R0 with the value of R0 in your test -*/
   // R0 = 73.63;
-  R0 = 34.23;
-  // R0 = 60.34;
+  R0 = 38.23;
+  // R0 = 96.87;
   ratio = RS_gas / R0;  // ratio = RS/R0
   double x = 0.4 * ratio;
   BAC = pow(x, -1.431);  //BAC in mg/L
@@ -98,6 +133,7 @@ float measureBAC() {
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN2, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);  // Set the buzzer pin as an output
   tone(BUZZER_PIN, 1000, 2000);
 
@@ -112,6 +148,7 @@ void setup() {
 // }
 
 const float BAC_TOLERANCE = 0.01;
+bool displayingBattery = false;
 void loop() {
   // Display the initial message
   display.clearDisplay();
@@ -121,19 +158,18 @@ void loop() {
   display.println("Press the button to");
   display.println("start measuring!");
   display.display();
-
-  if (isButtonPressed() && !measuringInProgress && !measurementCompleted) {
+  if (isButtonPressed1() && !measuringInProgress && !measurementCompleted) {
     // Set the measuring flag to true to start measuring
     measuringInProgress = true;
     measuringStartTime = millis();  // Record the start time of measuring
     delay(1000);                    // Wait for 1 second to avoid accidental double-clicks
 
     // Show "Measuring..." message for 5 seconds and perform the actual measurement
-    //  playBuzzerSound(3000, MEASURING_DURATION);
-    float forDelay;
+    // playBuzzerSound(3000, MEASURING_DURATION);
+    float forDelay; // opravi promenlivata
     forDelay = measureBAC();
     Serial.println(forDelay);
-    if (forDelay <= BAC_TOLERANCE) {
+    if (forDelay <= BAC_TOLERANCE) { // tuk da se napravi oshte edna proverka za da ne e nushno da se chaka tolkova
       Serial.println("yes");
       delay(2000);
     } else {
@@ -147,10 +183,15 @@ void loop() {
       display.display();
       delay(180000);
     }
+  } else if (isButtonPressed2() && !displayingBattery) {
+    // Display battery voltage for 5 seconds
+    displayingBattery = true;
+    batteryLevel();
+    displayingBattery = false;
   }
 
   // Check if the button is pressed to allow another measurement
-  if (isButtonPressed() && measurementCompleted) {
+  if (isButtonPressed1() && measurementCompleted) {
     // Reset the flags for a new measurement
     measuringInProgress = false;
     measurementCompleted = false;
--- test_for_real_breathanalyzer/test_for_real_breathanalyzer.ino
+++ test_for_real_breathanalyzer/test_for_real_breathanalyzer.ino
@@ -1,123 +1,124 @@
-// float sensor_volt;
-// float RS_gas;
-// float R0;
-// int R2 = 1000;
-
-// void setup() {
-//  Serial.begin(9600);
-// }
-
-// void loop() {
-//   int sensorValue = analogRead(A0);
-//   sensor_volt=(float)sensorValue/1024*5.0;
-//   RS_gas = ((5.0 * R2)/sensor_volt) - R2;
-//   R0 = RS_gas / 60;
-//   Serial.print("R0: ");
-//   Serial.println(R0);
-// }
-
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH1106.h>
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64
-#define OLED_RESET 4
-#define BUTTON_PIN 4
-Adafruit_SH1106 display(OLED_RESET);
-
-
 float sensor_volt;
 float RS_gas;
 float R0;
-float ratio;
-float BAC;
-int R2 = 1.02;
+int R2 = 1000;
 
-// set sensor to warm up and show it on display - remember to change the delay to higher
 void setup() {
-  Serial.begin(9600);
-  display.begin(SH1106_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  display.clearDisplay();
-  display.clearDisplay();
-  display.setTextSize(1.5);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 32);
-  Serial.println("MQ3 warming up!");
-  display.println("MQ3 warming up!");
-  display.display();
-  delay(2000);  // allow the MQ3 to warm up
-  display.clearDisplay();
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+ Serial.begin(9600);
 }
-
-// code for start meausoring
-byte buttonState;
-bool realTesting() {
-buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState == LOW) {
-    Serial.println("Button is pressed");
-    return true;
-  } else {
-    Serial.println("Button is not pressed");
-    return false;
-  }
-  delay(100);
-}
-
-// function for showing the message while measure.
 
 void loop() {
-   display.clearDisplay();
-    display.setTextSize(1.5);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 32);
-    // display.print("PPM ");
-    display.print("Press the button to start measuring! ");
-    display.display();
-  if (realTesting()) {
-      display.clearDisplay();
-    display.setTextSize(1.5);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 32);
-    // display.print("PPM ");
-    display.print("Measuring...");
-    display.display();
-    delay(3000);
-    while(true){
-      //
-      //
-      display.clearDisplay();
+  int sensorValue = analogRead(A1);
+  sensor_volt=(float)sensorValue/1024*5.0;
+  RS_gas = ((5.0 * R2)/sensor_volt) - R2;
+  R0 = RS_gas / 60;
+  Serial.print("R0: ");
+  Serial.println(R0);
+
+}
+
+// #include <SPI.h>
+// #include <Wire.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SH1106.h>
+// #define SCREEN_WIDTH 128  // OLED display width, in pixels
+// #define SCREEN_HEIGHT 64
+// #define OLED_RESET 4
+// #define BUTTON_PIN 4
+// Adafruit_SH1106 display(OLED_RESET);
 
 
-    int sensorValue = analogRead(A1);
-    Serial.println(sensorValue);
-    sensor_volt = (float)sensorValue / 1024 * 5.0;
-    RS_gas = ((5.0 * R2) / sensor_volt) - R2;
-    /*-Replace the value of R0 with the value of R0 in your test -*/
-    // R0 = 73.63;
-    // R0 = 110.34;
-    R0 = 30.23;
-    ratio = RS_gas / R0;  // ratio = RS/R0
-    double x = 0.4 * ratio;
-    BAC = pow(x, -1.431);  //BAC in mg/L
-    Serial.print("BAC = ");
-    Serial.print(BAC * 0.0001);  //convert to g/dL
-    Serial.print(" g/DL\n\n");
-    display.clearDisplay();
-    display.setTextSize(1.5);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 32);
-    // display.print("PPM ");
-    display.print("BAC = ");
-    display.print(BAC * 0.0001);
-    display.print(" g/DL\n\n");
-    display.display();
-    delay(1000);
-    }
+// float sensor_volt;
+// float RS_gas;
+// float R0;
+// float ratio;
+// float BAC;
+// int R2 = 1.02;
+
+// // set sensor to warm up and show it on display - remember to change the delay to higher
+// void setup() {
+//   Serial.begin(9600);
+//   display.begin(SH1106_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+//   display.clearDisplay();
+//   display.clearDisplay();
+//   display.setTextSize(1.5);
+//   display.setTextColor(WHITE);
+//   display.setCursor(0, 32);
+//   Serial.println("MQ3 warming up!");
+//   display.println("MQ3 warming up!");
+//   display.display();
+//   delay(2000);  // allow the MQ3 to warm up
+//   display.clearDisplay();
+//   pinMode(BUTTON_PIN, INPUT_PULLUP);
+// }
+
+// // code for start meausoring
+// byte buttonState;
+// bool realTesting() {
+// buttonState = digitalRead(BUTTON_PIN);
+//   if (buttonState == LOW) {
+//     Serial.println("Button is pressed");
+//     return true;
+//   } else {
+//     Serial.println("Button is not pressed");
+//     return false;
+//   }
+//   delay(100);
+// }
+
+// // function for showing the message while measure.
+
+// void loop() {
+//    display.clearDisplay();
+//     display.setTextSize(1.5);
+//     display.setTextColor(WHITE);
+//     display.setCursor(0, 32);
+//     // display.print("PPM ");
+//     display.print("Press the button to start measuring! ");
+//     display.display();
+//   if (realTesting()) {
+//       display.clearDisplay();
+//     display.setTextSize(1.5);
+//     display.setTextColor(WHITE);
+//     display.setCursor(0, 32);
+//     // display.print("PPM ");
+//     display.print("Measuring...");
+//     display.display();
+//     delay(3000);
+//     while(true){
+//       //
+//       //
+//       display.clearDisplay();
+
+
+//     int sensorValue = analogRead(A1);
+//     Serial.println(sensorValue);
+//     sensor_volt = (float)sensorValue / 1024 * 5.0;
+//     RS_gas = ((5.0 * R2) / sensor_volt) - R2;
+//     /*-Replace the value of R0 with the value of R0 in your test -*/
+//     // R0 = 73.63;
+//     // R0 = 110.34;
+//     R0 = 30.23;
+//     ratio = RS_gas / R0;  // ratio = RS/R0
+//     double x = 0.4 * ratio;
+//     BAC = pow(x, -1.431);  //BAC in mg/L
+//     Serial.print("BAC = ");
+//     Serial.print(BAC * 0.0001);  //convert to g/dL
+//     Serial.print(" g/DL\n\n");
+//     display.clearDisplay();
+//     display.setTextSize(1.5);
+//     display.setTextColor(WHITE);
+//     display.setCursor(0, 32);
+//     // display.print("PPM ");
+//     display.print("BAC = ");
+//     display.print(BAC * 0.0001);
+//     display.print(" g/DL\n\n");
+//     display.display();
+//     delay(1000);
+//     }
     
 
-  } else {
-    Serial.print("Nothing");
-  }
-}
+//   } else {
+//     Serial.print("Nothing");
+//   }
+// }
