ed2f95cdd3b67270f922ae52bb0f13bdaa0770d6
Daniel Dimitrov <dani.dimitrov.snack9@gmail.com>
2023-09-19 23:18:38 +0300

    Update codeDevBreathAn.ino

--- codeDevBreathAn/codeDevBreathAn.ino
+++ codeDevBreathAn/codeDevBreathAn.ino
@@ -203,25 +203,25 @@ void loop() {
 
 
 // for R0
-// float sensor_volt;
-// float RS_gas;
-// float R0;
-// // int R2 = 1000;
-// float R2 = 1.02;//не е проверено
+float sensor_volt;
+float RS_gas;
+float R0;
+// int R2 = 1000;
+float R2 = 1.02;//не е проверено
 
-// void setup() {
-//  Serial.begin(9600);
-// }
+void setup() {
+ Serial.begin(9600);
+}
 
-// void loop() {
-//   int sensorValue = analogRead(A1);
-//   sensor_volt=(float)sensorValue/1024*5.0;
-//   RS_gas = ((5.0 * R2)/sensor_volt) - R2;
-//   R0 = RS_gas / 60;
-//   Serial.print("R0: ");
-//   Serial.println(R0);
+void loop() {
+  int sensorValue = analogRead(A1);
+  sensor_volt=(float)sensorValue/1024*5.0;
+  RS_gas = ((5.0 * R2)/sensor_volt) - R2;
+  R0 = RS_gas / 60;
+  Serial.print("R0: ");
+  Serial.println(R0);
 
-// }
+}
 
 //FOR BUZZERS
 
